882548f5f703785f1699354c4923f7010e66aeb9
Layna Diepes <layna.bandeira.diepes@hotmail.com>
2023-08-07 09:30:56 -0300

    funcionando

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 
-function computerPlay(){
+function computerPlay() {
     const opcoes = ["rock", "paper", "scissors"];
-    const escolhaComputador = opcoes[Math.floor(Math.random() * opcoes.length)];
+    return opcoes[Math.floor(Math.random() * opcoes.length)];
 }
-let pontuacaoJogador = 0;
 
+function player() {
+    return prompt("please enter rock, paper or scissors!").trim().toLowerCase();
+}
 
-function begin() {
-
-    for (i = 0; i <= 5; i++) {
-
-        let gameBegin = confirm("let's play Rock, Paper, Scissors!");
-
-        if (gameBegin) {
-            let playerSelection = prompt("please enter rock, paper or scissors!").trim().toLowerCase();
-
-            if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-                alert("Opção inválida");
-                i--;
-            }
-            else {
-                if (playerSelection === escolhaComputador)
-                    alert("Empate / Computer: " + escolhaComputador + " Player: " + "playerSelection");
-                else if (playerSelection == "rock" && escolhaComputador == "paper" ||
-                    playerSelection == "paper" && escolhaComputador == "scissors" ||
-                    escolhaComputador == "rock" && playerSelection == "scissors")
-                    alert("Computer Wins! Computer: " + escolhaComputador + " Player: " + playerSelection);
-                else {
-                    alert("You win!! Computer: " + escolhaComputador + " Player: " + playerSelection);
-                    pontuacaoJogador++;
-                }
-            }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+        alert("Opção inválida");
+        i--;
+    }
+    else {
+        if (playerSelection === computerSelection)
+            alert("Empate / Computer: " + computerSelection + " Player: " + playerSelection);
+        else if (playerSelection == "rock" && computerSelection == "paper" ||
+            playerSelection == "paper" && computerSelection == "scissors" ||
+            computerSelection == "rock" && playerSelection == "scissors")
+            alert("Computer Wins! Computer: " + computerSelection + " Player: " + playerSelection);
+        else {
+            alert("You win!! Computer: " + computerSelection + " Player: " + playerSelection);
         }
     }
-
-    alert("Score " + pontuacaoJogador + "points");
-
 }
 
-begin();
+function playFiveRounds() {
+    let start = confirm("let's play Rock, Paper, Scissors!");
+    if (start) {
+        for(i=0; i<=5; i++){
+            const playerSelection = player();
+            playRound(playerSelection, computerSelection);
+        }    
+    }
+    else
+        alert("goodbye");
+}
+
+
+
+const computerSelection = computerPlay();
+playFiveRounds();
+
 
